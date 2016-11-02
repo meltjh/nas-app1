b8a4d4bda7e58c72af6e50bc99f808b11a0cdfe6
Melissa Tjhia <melissatjhia@MacBook-Pro-van-Melissa.local>
2016-11-02 17:23:18 +0100

    working app

--- melissatjhia-pset1/ViewController.swift
+++ melissatjhia-pset1/ViewController.swift
@@ -9,10 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Images
+    @IBOutlet weak var imageBody: UIImageView!
+    
+    @IBOutlet weak var imageEars: UIImageView!
+    
+    @IBOutlet weak var imageGlasses: UIImageView!
+    
+    @IBOutlet weak var imageHat: UIImageView!
+    
+    @IBOutlet weak var imageEyebrows: UIImageView!
+    
+    @IBOutlet weak var imageMouth: UIImageView!
+    
+    @IBOutlet weak var imageEyes: UIImageView!
+    
+    @IBOutlet weak var imageMustache: UIImageView!
+    
+    @IBOutlet weak var imageNose: UIImageView!
+    
+    @IBOutlet weak var imageShoes: UIImageView!
+    
+    @IBOutlet weak var imageArms: UIImageView!
+    
+    // Switches
+    @IBOutlet weak var switchArms: UISwitch!
+    
+    @IBOutlet weak var switchEars: UISwitch!
+    
+    @IBOutlet weak var switchEyebrows: UISwitch!
+    
+    @IBOutlet weak var switchEyes: UISwitch!
+    
+    @IBOutlet weak var switchGlasses: UISwitch!
+    
+    @IBOutlet weak var switchHat: UISwitch!
+    
+    @IBOutlet weak var switchMouth: UISwitch!
+    
+    @IBOutlet weak var switchMustache: UISwitch!
+    
+    @IBOutlet weak var switchNose: UISwitch!
 
+    @IBOutlet weak var switchShoes: UISwitch!
+    
+    var switchImageRelations = [String : UIImageView!]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       switchImageRelations = [switchArms.restorationIdentifier!: imageArms, switchEars.restorationIdentifier!: imageEars, switchEyebrows.restorationIdentifier!: imageEyebrows, switchEyes.restorationIdentifier!: imageEyes, switchGlasses.restorationIdentifier!: imageGlasses, switchHat.restorationIdentifier!: imageHat, switchMouth.restorationIdentifier!: imageMouth, switchMustache.restorationIdentifier!: imageMustache, switchNose.restorationIdentifier!: imageNose, switchShoes.restorationIdentifier!: imageShoes]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +68,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeParts(_ sender: UISwitch!) {
+        
+        switchImageRelations[sender.restorationIdentifier!]!.isHidden = !sender.isOn
+    }
 
 }
 
